dc39e182a93e1317b585bb352be1f4bfadd9f505
Scott Lee <yle4@cdc.gov>
2018-01-23 11:20:08 -0500

    style edits

--- lda.py
+++ lda.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import numpy as np
-import lda
-import tools
 import argparse
 
-from tools import *
 from sklearn.svm import LinearSVC, SVC
 from sklearn.decomposition import LatentDirichletAllocation as LDA
+
+import tools
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	
-	#positional arguments
+	# Positional arguments
 	parser.add_argument('data', default='~/data/addm/corpus_with_lemmas.csv', help='path for the input data')
 	parser.add_argument('x_name', default='dx', help='name of the column holding the text')
 	parser.add_argument('y_name', default='aucaseyn', help='name of the column holding the target values')
 
-	#optional arguments for tuning
+	# Optional arguments for tuning
 	parser.add_argument('-lm', '--limit_features', type=bool, default=True, help='limit the number of features for the SVM? (yes or no)')
 	parser.add_argument('-ft', '--features', type=int, default=35000, help='number of features for the SVM, if limited')
 	parser.add_argument('-ng', '--ngrams', type=int, default=2, help='max ngram size')
@@ -27,34 +26,32 @@ if __name__ == '__main__':
 	parser.add_argument('-vb', '--verbose', default=True, help='should functions print updates as they go?')
 	args = parser.parse_args()
 
-	#loading and processing the data
+	# Loading and processing the data
 	df = pd.read_csv(args.data)
-	d = TextData()
+	d = tools.TextData()
 	if args.limit_features:
 		d.process(df, args.x_name, args.y_name, max_features=args.features, verbose=args.verbose)
 	else:
 		d.process(df, args.x_name, args.y_name, max_features=None, verbose=args.verbose)
 	
-	#fitting the LDA model
+	# Fitting the LDA model
 	if args.verbose:
 		print "Fitting the LDA model..."
 	lda = LDA(learning_method='online')
 	topics = lda.fit_transform(d.X)
 	
-	#feeding the LDA matrix back to the TextData() object
+	# Feeding the LDA matrix back to the TextData() object
 	d.set_xy(topics, d.y)
 	
-	#getting the training and test sets	
+	# Getting the training and test sets	
 	if args.verbose:
 		print "Splitting the data into training and test sets..."
 	d.split(args.split_method, args.split_variable, args.test_value)
 	
-	#modeling setting up the classifier
+	# Modeling setting up the classifier
 	if args.verbose:
 		print "Training the SVM..."
 	clf = LinearSVC()
 	clf.fit(d.X_train, d.y_train)
 	acc = clf.score(d.X_test, d.y_test)
 	print "\nResults:\nAccuracy with the SVM on LDA is %0.4f" %acc
-	
-	
