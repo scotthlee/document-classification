a8e5c38152502717b129ea033cc3c403d7899b55
Scott Lee <yle4@cdc.gov>
2018-04-04 13:28:20 -0400

    style edits

--- tools.py
+++ tools.py
@@ -18,7 +18,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
 
 """Stuff for preparing the data"""
-#divides the data into training and testing samples using a column variable
+# Divides the data into training and testing samples using a column variable
 def split_by_var(x, y, full_set, split_var, test_val):
 	v = full_set[split_var].astype(str)
 	train_indices = v[~v.isin([test_val])].index.tolist()
@@ -29,16 +29,17 @@ def split_by_var(x, y, full_set, split_var, test_val):
 	y_test = y[test_indices]
 	return [X_train, X_test, y_train, y_test]
 
-#class for holding the split training and test data
+# Class for holding the split training and test data
 class TextData:
 	def __init__(self):
-	#setting attributes for the data
+	# Setting attributes for the data
 		self.data = pd.DataFrame()
 		self.X, self.y = [], []
 		self.X_train, self.X_test = [], []
 		self.y_train, self.y_test = [], []
 
-	#wrappers for saving data frame to the RF object for when process() isn't used
+	# Wrappers for saving data frame to the RF 
+    # object for when process() isn't used
 	def set_data(self, df):
 		self.data = df
 		return
@@ -52,20 +53,34 @@ class TextData:
 		self.y = deepcopy(y)
 		return
 
-	#another wrapper for the vectorization functions; optional, and will take a while
-	def process(self, df, x_name, y_name=None, ngrams=2, max_features=35000, method='counts', binary=True, sparse=False):
-		#choosing the particular flavor of vectorizer
+	# Another wrapper for the vectorization functions; 
+    # optional, and will take a while
+	def process(self, 
+             df, 
+             x_name, 
+             y_name=None, 
+             ngrams=2, 
+             max_features=35000, 
+             method='counts', 
+             binary=True, 
+             sparse=False):
+		# Choosing the particular flavor of vectorizer
 		if method == 'counts':
-			vectorizer = CountVectorizer(max_features=max_features, ngram_range=(1, ngrams), decode_error='replace', binary=binary)
+			vectorizer = CountVectorizer(max_features=max_features, 
+                                ngram_range=(1, ngrams), 
+                                decode_error='replace', 
+                                binary=binary)
 		elif method == 'tfidf':
-			vectorizer = TfidfVectorizer(max_features=max_features, ngram_range=(1, ngrams), decode_error='replace')
+			vectorizer = TfidfVectorizer(max_features=max_features, 
+                                ngram_range=(1, ngrams),
+                                decode_error='replace')
 
-		#fitting the vectorizer and converting the counts to an array
+		# Fitting the vectorizer and converting the counts to an array
 		full_fit = vectorizer.fit_transform(df[x_name])
 		full_counts = full_fit.toarray()
 		self.vocabulary_ = vectorizer.vocabulary_
 
-		#passing the attributes up to the class instance
+		# Passing the attributes up to the class instance
 		self.data = df
 		if sparse:
 			full_counts = csr_matrix(full_counts)
@@ -74,29 +89,39 @@ class TextData:
 			self.y = np.array(df[y_name])
 		return
 
-	#splits the data into training and test sets; either called from process()
-	#or on its own when your text is already vectorized and divided into x and y
-	def split(self, method='train-test', split_var=None, test_val=None, seed=None):
+	# Splits the data into training and test sets;
+	def split(self, 
+           method='train-test', 
+           split_var=None, 
+           test_val=None, 
+           seed=None):
 		if method == 'train-test':
-			self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, random_state=seed)
+			self.X_train, self.X_test, 
+            self.y_train, self.y_test = train_test_split(self.X, self.y, 
+                                                         random_state=seed)
 		elif method == 'var':
-			self.X_train, self.X_test, self.y_train, self.y_test = split_by_var(self.X, self.y, data,
+			self.X_train, self.X_test, 
+            self.y_train, self.y_test = split_by_var(self.X, self.y, data,
 												split_var, test_val)
 		elif method == 'bootstrap':
 			index_range = np.arange(0, len(self.y))
 			indices = np.random.choice(index_range, len(self.y), replace=True)
 			X, y = self.X[indices, :], self.y[indices]
-			self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, random_state=seed)
+			self.X_train, self.X_test, 
+            self.y_train, self.y_test = train_test_split(X, y, 
+                                                         random_state=seed)
 		return
 
-#wrapper for inspect.getargspec; helps navigate my less-than-ideal naming conventions
+# Wrapper for inspect.getargspec; helps navigate my 
+# less-than-ideal naming conventions
 def get_args(foo):
 	return inspect.getargspec(foo)
 
 """General ML functions"""
-#produces all the accuracy statistics
+# Produces all the accuracy statistics
 def diagnostics(guesses, targets):
-	out = pd.DataFrame(np.zeros([1, 5]), columns=['se', 'sp', 'ppv', 'f1', 'acc'])
+	out = pd.DataFrame(np.zeros([1, 5]), 
+                    columns=['se', 'sp', 'ppv', 'f1', 'acc'])
 	out['se'] = sens(guesses, targets)
 	out['sp'] = spec(guesses, targets)
 	out['ppv'] = ppv(guesses, targets)
@@ -135,8 +160,8 @@ def f1(guesses, targets):
 def acc(guesses, targets):
 	return np.true_divide(np.sum(guesses == targets), len(guesses))
 
-#returns the predicted outputs based on inputs, training weights, and training bias
-#exp=True will exponentiate the predicted values, transforming to [0, 1]
+# Returns the predicted outputs based on inputs, training weights, and bias
+# exp=True will exponentiate the predicted values, transforming to [0, 1]
 def linear_prediction(x, w, b, neg=0, binary=True):
 	guesses = np.matmul(x, w.transpose()) + b
 	if binary:
@@ -147,7 +172,8 @@ def linear_prediction(x, w, b, neg=0, binary=True):
 		prediction = guesses
 	return prediction
 
-#returns the accuracy of a classifier based on inputs, outputs, training weights, and training bias
+# Returns the accuracy of a classifier based on inputs, 
+# outputs, training weights, and training bias
 def accuracy(x, y, w, b):
     guess = linear_prediction(x, w, b)
     return np.true_divide(np.sum(guess.reshape(y.shape) == y), x.shape[0])
